68efbef2aa3fbbcde8329bc453f6131fa3e97005
Reuno92 <racinet.renaud@orange.fr>
2016-10-11 20:14:25 +0200

    correction gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         dirs: {
             srcJs  : 'src/js/',
-            destJS : 'asset/js/'
+            destJs : 'asset/js/'
         },
 
         concat: {
